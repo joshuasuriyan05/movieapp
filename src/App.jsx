5f26f5bda3b9a9fa7e1eca38948bd014d83f27b5
jowhuq <joshuasuriyan@karunya.edu.in>
2025-05-28 19:59:24 +0530

    icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {FaHeart, FaRegHeart} from 'react-icons/fa';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 //import './App.css';
 import Navbar from './assets/pages/Navbar.jsx';
