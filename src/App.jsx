774430973d033ce88ffcd0e023362bf130745d58
jowhuq <joshuasuriyan@karunya.edu.in>
2025-05-28 19:56:59 +0530

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 //import './App.css';
 import Navbar from './assets/pages/Navbar.jsx';
 import Home from './assets/pages/Home.jsx';
+import Moviecard from './assets/components/Moviecard.jsx';
 import WatchList from './assets/pages/WatchList.jsx';
 import Generes from './assets/components/Generes.jsx';
 import {WatchListProvider} from './assets/context/WatchListContext.jsx';
